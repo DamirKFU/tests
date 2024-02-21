89cc6ba115e6778025372810a361c42bdfcf55e3
Shalltear-ov <Damir.SakhbievGG@gmail.com>
2024-02-21 15:00:04 +0300

    add new function

--- tests.py
+++ tests.py
@@ -1,25 +1,27 @@
 import unittest
 from main import Solution
 
+
 class TestStringMethods(unittest.TestCase):
     def test_rims_fucntion(self):
         tests = [
-            (4444, "MMMMCDXLIV"),
+            (4444, "MMMMCDXLI"),
             (4, "IV"),
             (9, "IX"),
             (40, "XL"),
             (90, "XC"),
             (400, "CD"),
             (900, "CM"),
-            (237, "CCXXXVII")
+            (237, "CCXXXVII"),
         ]
         for value, expected in tests:
             with self.subTest(value=value):
-                self.assertEqual(Solution.romanToInt(value), expected, msg="тест на риские цифры не пройден")
+                self.assertEqual(
+                    Solution.romanToInt(value),
+                    expected,
+                    msg="тест на риские цифры не пройден",
+                )
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
